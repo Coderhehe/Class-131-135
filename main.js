a5924bc64a03386aa50b85b7ae194cd40d0288c9
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-08-04 20:32:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+img="";
+object=[];
+status="";
+
+
+function setup() {
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    object=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status:Object Detecting";
+}
+function modelLoaded() {
+    console.log("Model Is Loaded");
+    status=true;
+    object.detect(video,gotResult);
+}
+function gotResult(error,result){
+if (error) {
+    console.error(error);
+}
+else{
+    console.log(result);
+    object=result;
+}
+}
+
+function draw() {
+    image(video,0,0,640,420);
+r=random(255);
+g=random(255);
+b=random(255);
+    if(status!=""){
+for (i=0; i < object.length; i++)  {
+    document.getElementById("status").innerHTML="Status:Objects Detected ";
+    document.getElementById("nobject").innerHTML="Number Of Objects Dectected:  "+object.length;
+ fill(r,g,b); 
+ percent=floor(object[i].confidence * 100);
+ text(object[i].label+" "+percent+"% ",object[i].x+15,object[i].y+15);
+ noFill();
+ stroke(r,g,b);
+ rect(object[i].x,object[i].y,object[i].width,object[i].height );  
+}
+    }
+}
